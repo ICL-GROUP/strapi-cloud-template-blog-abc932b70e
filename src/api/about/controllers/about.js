ae4cc1823be00ba30c9b795ee6b5e0dc76d06ae9
Hadassa Lugasi <Hadassa.Lugasi@icl-group.com>
2024-06-02 10:41:46 +0300

    add find function to about controller

--- src/api/about/controllers/about.js
+++ src/api/about/controllers/about.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::about.about',{
+    async find(ctx) {
+        try {
+          // Retrieve all instances of the 'about' model
+          const about = await strapi.services.about.find();
+    
+          // Return the retrieved data
+          return ctx.send(about);
+        } catch (error) {
+          // Handle any errors that occur during the retrieval process
+          return ctx.badRequest('Failed to retrieve about information');
+        }
+      }
+});
